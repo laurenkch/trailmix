daf3c7fd882006bfa54ba1d9e8340c276199787f
laurenkch <lkoch879@gmail.com>
2022-03-21 11:01:22 -0400

    testing images on heroku

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,5 +6,5 @@ app_name = 'index'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('<path:resource>', IndexView.as_view())
+    path('<path:resource>/', IndexView.as_view())
 ]
